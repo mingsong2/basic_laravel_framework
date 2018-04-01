67c0ecea745ca1de7a97435c714bbca0f2a3a084
frank <13732629211@163.com>
2018-04-01 21:41:51 +0800

    no message

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Student;
+use Illuminate\Container\Container;
+class WelcomeController
+{
+    public function index()
+    {
+        $student = Student::first();
+        $data = $student->toArray();
+        
+        $app = Container::getInstance();
+        $factory = $app->make('view');
+
+        return $factory->make('welcome')->with('data',$data);
+    }
+}
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php 
+return [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'lara',
+    'username'  => 'root',
+    'password'  => 'songming43',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_general_ci',
+    'prefix'    => ''
+];
